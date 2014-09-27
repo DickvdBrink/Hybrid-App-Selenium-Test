51dab2225c19244b0df560d13842784b6c45391b
Dick van den Brink <d_vandenbrink@outlook.com>
2014-09-27 18:29:28 +0200

    Post istanbul coverage object to istanbul-middleware driver.executeScript function fails for now.

--- test/open-app.js
+++ test/open-app.js
@@ -8,6 +8,26 @@ var executor = executors.createExecutor("http://localhost:4444/wd/hub");
 var capabilities = new webdriver.Capabilities({ aut: "io.github.dvdbrink.hybridselenium:0.0.1" });
 var driver = webdriver.WebDriver.createSession(executor, capabilities);
 
+test.afterEach(function (done) {
+    this.timeout(10000);
+    driver.executeScript("return window.__coverage__;").then(function (coverage) {
+        var options = {
+            host: "localhost",
+            port: 4488,
+            path: "/coverage/client",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+        var req = http.request(options, function (res) {
+            done();
+        });
+        req.write(JSON.stringify(coverage));
+        req.end();
+    });
+});
+
 test.describe('Open app', function() {
     this.timeout(15000);
     driver.switchTo().window("WEBVIEW");
